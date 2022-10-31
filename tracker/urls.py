871eb3df54233f49edba823b4953a6cb666d6ace
DanyDery <dery.dany@gmail.com>
2022-10-31 17:58:44 +0200

    better User Experience integrating it with the frontend

--- tracker/urls.py
+++ tracker/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.collector, name='collector'),
+    path('tracking/', views.tracker, name='tracking'),
+
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 
-def index(request):
+def collector(request):
+    return render(request, 'collector.html')
+
+def tracker(request):
     return render(request, 'tracker.html')
+
+
